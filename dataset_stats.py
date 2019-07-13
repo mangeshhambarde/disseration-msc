d283d2c9b4626d4f796cb53c5001ffff80d9ef9f
Mangesh Hambarde <mhambarde1@sheffield.ac.uk>
2019-07-13 21:30:48 +0100

    Update

--- dataset_stats.py
+++ dataset_stats.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import argparse
+import glob
 import os
+import statistics
 import matplotlib.pyplot as plt
 
 """
@@ -19,20 +21,29 @@ Currently shown:
     Hierarchy should be like... dataset_dir/data/single_channel...
 """
 
-def percentage_speech(uem_file, sad_dir):
-    rec_percentages = {}
+def rec_id_to_name(rec_id):
+    return "DH_{:04d}".format(rec_id)
 
-    # calculate durations.
+def rec_name_to_id(rec_name):
+    return int(rec_name[3:])
+
+def get_recording_durations(uem_file):
     rec_durations = {}
+    rec_id = 1
     with open(uem_file) as f:
         for line in f:
-            rec_id = line.split()[0]
             rec_durations[rec_id] = float(line.split()[-1])
+            rec_id += 1
+    return rec_durations
+
+def get_percentage_speech(uem_file, sad_dir):
+    rec_durations = get_recording_durations(uem_file)
 
     # calculate percentages.
+    rec_percentages = {}
     for rec_id in rec_durations:
         speech_duration = 0.
-        sad_file = os.path.join(sad_dir, rec_id+".lab")
+        sad_file = os.path.join(sad_dir, rec_id_to_name(rec_id)+".lab")
         with open(sad_file) as f:
             for line in f:
                 cols = line.split()
@@ -41,16 +52,45 @@ def percentage_speech(uem_file, sad_dir):
 
     return rec_percentages
 
+def get_avg_speaker_turn_length(uem_file, rttm_dir):
+    rec_ids = get_recording_durations(uem_file)
+
+def get_num_speakers(rttm_dir):
+    speaker_count = {}
+    for rttm_file in sorted(glob.glob(rttm_dir + "/DH*rttm")):
+        speakers = set()
+        with open(rttm_file) as f:
+            for line in f:
+                speakers.add(line.split()[7])
+        basename = os.path.basename(rttm_file)
+        rec_name = os.path.splitext(basename)[0]
+        speaker_count[rec_name_to_id(rec_name)] = len(speakers)
+    return speaker_count
+
 def print_all(flac_dir, rttm_dir, sad_dir, uem_file):
-    rec_percentages = percentage_speech(uem_file, sad_dir)
-
-    for rec_id, per in rec_percentages.items():
-        print(rec_id, per)
-
-    plt.plot(range(1,len(rec_percentages)+1), rec_percentages.values())
+    # Percentage speech.
+    rec_percentages = get_percentage_speech(uem_file, sad_dir)
+    plt.plot(rec_percentages.keys(), rec_percentages.values())
     plt.xlabel("Recording ID")
     plt.ylabel("% speech")
     plt.savefig("percentage_speech.png")
+
+    # Length of recordings.
+    rec_durations = get_recording_durations(uem_file)
+    plt.clf()
+    plt.plot(rec_durations.keys(), rec_durations.values())
+    plt.xlabel("Recording ID")
+    plt.ylabel("Length of recording (sec)")
+    plt.savefig("recording_lengths.png")
+
+    # Number of speakers.
+    speaker_count = get_num_speakers(rttm_dir)
+    plt.clf()
+    plt.plot(speaker_count.keys(), speaker_count.values())
+    plt.xlabel("Recording ID")
+    plt.ylabel("Number of speakers")
+    plt.savefig("num_speakers.png")
+    print("Average number of speakers = ", statistics.mean(speaker_count.values()))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
